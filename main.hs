59e8e26ad604c774f15888e9dd244e1fbba94930
Gianmarco <gianmarco.bigliardi@studenti.unipr.it>
2025-10-23 09:31:38 +0200

    reso il gioco da terminale interattivo con scanl e takewhile

--- main.hs
+++ main.hs
@@ -1,33 +1,37 @@
 module Main where
 
 import Akari
-import Control.Monad (when)
 
-applyMoves :: Board -> [(Int, Int)] -> Board
-applyMoves = foldl (\b (x, y) -> illuminateBoard (toggleLampadina x y b))
+applyMove :: Board -> (Int, Int) -> Board
+applyMove b (x, y) = illuminateBoard (toggleLampadina x y b)
+
+parseMove :: String -> (Int, Int)
+parseMove line =
+  let [sx, sy] = words line
+  in (read sx, read sy)
 
 main :: IO ()
 main = do
   putStrLn "=== AKARI Puzzle ==="
   content <- readFile "boards/board7x7.1"
   let initialBoard = illuminateBoard (parseBoard content)
-  putStrLn "\nBoard iniziale:"
-  loop initialBoard []
 
-loop :: Board -> [(Int, Int)] -> IO ()
-loop initialBoard moves = do
-  let board = applyMoves initialBoard moves
-  putStrLn (boardToString (illuminateBoard board))
-  if isComplete board
-    then putStrLn "*** Gioco completato! Complimenti! ***"
-    else do
-      putStrLn "Inserisci mossa (x y) oppure 'q' per uscire:"
-      line <- getLine
-      when (not $ null line) $ do
-        if line == "q"
-          then putStrLn "*** Gioco terminato. ***"
-          else do
-            let [sx, sy] = words line
-                x = read sx
-                y = read sy
-            loop initialBoard (moves ++ [(x, y)])
+  putStrLn "\nBoard iniziale:"
+  putStrLn (boardToString initialBoard)
+
+  putStrLn "\nInserisci mosse (x y), una per riga. Digita Ctrl+D (Unix) o Ctrl+Z (Windows) per terminare.\n"
+
+  input <- getContents
+
+  let moves = map parseMove (lines input)
+
+      states = scanl applyMove initialBoard moves
+
+      playing = takeWhile (not . isComplete) states
+
+  mapM_ (\b -> do
+           putStrLn "\nBoard aggiornata:"
+           putStrLn (boardToString b)
+        ) playing
+
+  putStrLn "\n*** Gioco terminato o completato! ***"
